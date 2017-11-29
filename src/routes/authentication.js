317a1083b22a5f8a6e1d8403b185b02c01f189bd
Jay Cribas <feverjay@gmail.com>
2017-11-29 13:52:51 -0800

    Writes forms, routes and queries for user authentication

---
+++ src/routes/authentication.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import {signUp, signIn} from '../actions'
+
+const router = express.Router()
+
+router.get('/sign-up', (req, res) => {
+  res.render(
+    'authentication/sign-up',
+    {warning: ''},
+  )
+})
+
+router.post('/sign-up', (req, res) => {
+  signUp(req.body)
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(() => {
+      res.render(
+        'authentication/sign-up',
+        {warning: 'Blank field or email already exists'},
+      )
+    })
+})
+
+router.get('/sign-in', (req, res) => {
+  res.render(
+    'authentication/sign-in',
+    {warning: ''},
+  )
+})
+
+router.post('/sign-in', (req, res) => {
+  signIn(req.body)
+    .then(() => {
+      res.redirect('/')
+    })
+    .catch(() => {
+      res.render(
+        'authentication/sign-in',
+        {warning: 'Invalid email or password.'},
+      )
+    })
+})
+
+export default router
