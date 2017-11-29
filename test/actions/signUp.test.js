8412eef0dea1c4877d6196073e5e934cc3d02028
Jay Cribas <feverjay@gmail.com>
2017-11-29 14:42:03 -0800

    resaves passing test of signUp

--- test/actions/signUp.test.js
+++ test/actions/signUp.test.js
@@ -4,7 +4,13 @@ import { signUp } from '../../src/actions';
 
 describe('function signUp ', () => {
   it('should create a new row in the users table', () => {
-    // TODO: FIXME and write a real test
-    expect(1).to.equal(2)
+    const user = {name: 'Testrinal', email: 'testrinal@test.com', password: '123'}
+    return signUp(user)
+      .then((newUser) => {
+        expect(newUser.name).to.equal('Testrinal')
+        expect(newUser.email).to.equal('testrinal@test.com')
+        expect(newUser.password).to.equal('123')
+        expect(newUser).to.have.all.keys('id', 'name', 'email', 'password')
+      })
   })
 })
