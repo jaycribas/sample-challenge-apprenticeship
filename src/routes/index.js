6683c79b9028561838610a3f98df5a7511a008ca
Jay Cribas <feverjay@gmail.com>
2017-11-30 15:20:52 -0800

    Edit user (#4)
    * Writes profile page and routes
    
    * Adds hidden id input to update form
    
    * Writes AJAX (and fetch) PUT request to update profile

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import albums from './albums'
+import users from './users'
 import authentication from './authentication'
 
 const routes = express.Router()
@@ -8,5 +9,6 @@ const routes = express.Router()
 routes.use('/', authentication)
 routes.get('/', (req, res) => res.redirect('/albums'))
 routes.use('/albums', albums)
+routes.use('/users', users)
 
 export default routes
