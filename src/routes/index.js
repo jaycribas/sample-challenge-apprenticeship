0a80fb0ac1037b170047b48d010f03514412cd5b
Jay Cribas <feverjay@gmail.com>
2017-11-30 17:48:53 -0800

    Saves user session on successful signup and signin

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import session from 'express-session'
 
 import albums from './albums'
 import users from './users'
@@ -6,7 +7,15 @@ import authentication from './authentication'
 
 const routes = express.Router()
 
+routes.use(session({
+  key: 'user_sid',
+  secret: 'shh top secret',
+  resave: false,
+  saveUninitialized: false,
+}))
+
 routes.use('/', authentication)
+
 routes.get('/', (req, res) => res.redirect('/albums'))
 routes.use('/albums', albums)
 routes.use('/users', users)
